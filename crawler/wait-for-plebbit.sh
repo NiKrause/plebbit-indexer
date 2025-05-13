337a7d670b94f3c8f2057a5ce6f227fb117605cf
Nico Krause <post@nicokrause.com>
2025-05-13 12:04:28 +0500

    cleanup

--- crawler/wait-for-plebbit.sh
+++ crawler/wait-for-plebbit.sh
@@ -6,5 +6,6 @@ while [ ! -f /app/data/auth_key ]; do
 done
 export PLEBBIT_AUTH_KEY=$(cat /app/data/auth_key)
 echo "Found auth key: $PLEBBIT_AUTH_KEY"
-
+export PLEBBIT_WS_URL=ws://plebbit-cli:9138/$PLEBBIT_AUTH_KEY
+echo "connecting to $PLEBBIT_WS_URL"
 npm start
