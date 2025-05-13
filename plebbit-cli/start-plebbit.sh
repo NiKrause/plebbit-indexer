6d3c270463a4f05f17b23391d686eb9752dd734e
Nico Krause <post@nicokrause.com>
2025-05-13 12:38:54 +0500

    using /app/data datapath

--- plebbit-cli/start-plebbit.sh
+++ plebbit-cli/start-plebbit.sh
@@ -4,7 +4,7 @@ echo "Cleaning up old files..."
 rm -f /app/data/plebbit.log /app/data/auth_key
 
 echo "Starting plebbit-cli daemon..."
-/app/plebbit-cli/bin/run daemon 2>&1 | tee /app/data/plebbit.log &
+/app/plebbit-cli/bin/run daemon --plebbitOptions.dataPath /app/data 2>&1 | tee /app/data/plebbit.log &
 
 while true; do
   if grep -q "plebbit rpc: listening on.*secret auth key for remote connections" /app/data/plebbit.log; then
@@ -14,15 +14,8 @@ while true; do
     echo "Attempting to extract auth key..."
     AUTH_KEY=$(grep "plebbit rpc: listening on.*secret auth key for remote connections" /app/data/plebbit.log | tail -n 1 | sed -n 's/.*ws:\/\/[^:]*:[0-9]*\/\/\([^ ]*\).*/\1/p')
     echo "Extracted AUTH_KEY: $AUTH_KEY"
-    if [ -n "$AUTH_KEY" ]; then
-      echo "$AUTH_KEY" > /app/data/auth_key
-      echo "Successfully extracted auth key: $AUTH_KEY"
-      break
-    else
-      echo "Failed to extract auth key, retrying..."
-    fi
+    break;
   fi
   sleep 1
 done
 
-wait
