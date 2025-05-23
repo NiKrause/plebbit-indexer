b829e0bcbb40e936c7bac6f1429c9c8a830c9b20
Nico Krause <post@nicokrause.com>
2025-05-23 17:19:53 +0500

    fixed lint error

--- plebindex/src/app/post/replies.tsx
+++ plebindex/src/app/post/replies.tsx
@@ -1,5 +1,4 @@
 import { Suspense } from 'react';
-import Link from 'next/link';
 import { fetchPost, fetchReplies } from '../../api/posts';
 import { Post, Reply } from '../../types';
 import { formatTimestamp } from '../../utils/formatting';
