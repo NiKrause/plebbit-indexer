6789c0599cc1658271b14aca549badb7e93e9397
Nico Krause <post@nicokrause.com>
2025-06-07 15:09:30 +0500

    changed logo

--- plebindex/src/app/plebscanlogo.tsx
+++ plebindex/src/app/plebscanlogo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function PlebscanLogo() {
   return (
     <Image
-      src="/plebscan-logo.jpg" // or .png if that's what you saved
+      src="/plebscan-logo.png" // or .png if that's what you saved
       alt="Plebscan Logo"
       width={40}
       height={40}
