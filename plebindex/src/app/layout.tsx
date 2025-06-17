e1ca3f51b4974a1eda25296f934af49ca4d41076
Nico Krause <post@nicokrause.com>
2025-06-17 20:45:44 +0200

    adding footer

--- plebindex/src/app/layout.tsx
+++ plebindex/src/app/layout.tsx
@@ -44,13 +44,25 @@ export default function RootLayout({
       <head>
         <link rel="alternate" type="application/xml" href="/sitemap.xml" title="Sitemap" />
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable} min-h-screen p-4 md:p-8`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} min-h-screen flex flex-col`}>
         <div className="fixed bottom-0 left-0 left-0 z-[9999] p-8 md:p-16 px-4 m-8">
            <CookieConsent />
         </div>
-        <div className="container mx-auto">
+        <div className="fixed bottom-0 left-0 left-0 z-[9999] p-8 md:p-16 px-4 m-8">
+           <CookieConsent />
+        </div>
+        <div className="flex-grow container mx-auto px-4 pb-40">
           {children}
         </div>
+        <footer className="fixed bottom-0 left-0 right-0 py-4 text-center text-xs text-gray-500 border-t z-[9998] bg-white">
+          <div className="container mx-auto px-4">
+            <p className="text-center">
+              Plebscan test is a search engine for Plebbit, a decentralized P2P Reddit alternative. 
+              All content is user-generated and we are not responsible for any content posted by users. 
+              Plebbit aims to provide a censorship-resistant platform similar to Reddit, but with enhanced privacy and decentralization features.
+            </p>
+          </div>
+        </footer>
         <Analytics />
       </body>
     </html>
